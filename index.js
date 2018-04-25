4462a635f940bdde032d0ec55a217765903d1bc8
Vu Tri Cong <vutricongbka@gmail.com>
2018-04-25 21:47:33 +0700

    show my webcam

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app= express();
+app.set('view engine','ejs');
+app.set('views','./views');
+app.get('/',(req,res)=>res.render('home'));
+app.listen(3000,()=>console.log('Server start add: 3000'));
